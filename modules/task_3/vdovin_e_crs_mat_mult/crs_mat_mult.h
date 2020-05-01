83cbc7981f02da8c18ad49098ad394687ada7926
Eugene <32429730+J-win@users.noreply.github.com>
2020-05-02 00:39:22 +0300

    add task3

---
+++ modules/task_3/vdovin_e_crs_mat_mult/crs_mat_mult.h
@@ -0,0 +1,62 @@
+// Copyright 2020 Vdovin Eugene
+#ifndef MODULES_TASK_3_VDOVIN_E_CRS_MAT_MULT_CRS_MAT_MULT_H_
+#define MODULES_TASK_3_VDOVIN_E_CRS_MAT_MULT_CRS_MAT_MULT_H_
+
+#include <tbb/tbb.h>
+#include <vector>
+#include <complex>
+#include <cmath>
+#include <random>
+#include <algorithm>
+#include <utility>
+#include <numeric>
+
+class CRSMatrix {
+public:
+    int n;
+    int nz;
+    int omp_k;
+
+    std::vector<std::complex<double>> value;
+    std::vector<int> col;
+    std::vector<int> rowindex;
+
+    CRSMatrix multiplicate(const CRSMatrix &mtx) const;
+    bool findElemInVector(const std::vector<std::pair<int, std::complex<double>>>& vec, const int elem);
+
+    explicit CRSMatrix(const int n_ = 0, const int nz_ = 0);
+    CRSMatrix(const int n_, const int nz_,
+        const std::vector<std::complex<double>> &value_,
+        const std::vector<int> col_,
+        const std::vector<int> rowindex_);
+    CRSMatrix(const CRSMatrix &mtx);
+    ~CRSMatrix() {}
+
+    CRSMatrix operator*(const CRSMatrix &mtx) const;
+    CRSMatrix& operator=(const CRSMatrix &mtx);
+    CRSMatrix transpose() const;
+    void buildRandomCRSMatrix();
+    void getThreads(int numTreads);
+
+    bool operator==(const CRSMatrix &mtx) const;
+    bool operator!=(const CRSMatrix &mtx) const;
+};
+
+class Multiplicator {
+public:
+    const CRSMatrix* a;
+    const CRSMatrix* b;
+
+    std::vector<std::complex<double>>* value_res;
+    std::vector<int>* col_res;
+    int* rownz;
+
+    Multiplicator(const CRSMatrix* a_, const CRSMatrix* b_,
+        std::vector<std::complex<double>>* value_res_,
+        std::vector<int>* col_res_,
+        int* rownz_) : a(a_), b(b_), value_res(value_res_), col_res(col_res_), rownz(rownz_) {}
+
+    void operator() (const tbb::blocked_range<int>& r) const;
+};
+
+#endif  // MODULES_TASK_3_VDOVIN_E_CRS_MAT_MULT_CRS_MAT_MULT_H_
---
+++ modules/task_3/vdovin_e_crs_mat_mult/main.cpp
@@ -0,0 +1,100 @@
+// Copyright 2020 Vdovin Eugene
+#include <gtest/gtest.h>
+#include <tbb/tbb.h>
+#include <omp.h>
+#include <vector>
+#include <complex>
+#include <cmath>
+#include "./crs_mat_mult.h"
+
+TEST(CRSMatrix, test_copy_1) {
+  std::vector<std::complex<double>> v = { 1, 2, 3, 4, 8, 5, 7, 1, 6 };
+  std::vector<int> c = { 0, 4, 2, 3, 3, 5, 1, 2, 5 };
+  std::vector<int> r = { 0, 2, 4, 4, 6, 6, 9 };
+
+  CRSMatrix a(6, 9, v, c, r);
+  CRSMatrix b(6, 9, v, c, r);
+
+  EXPECT_EQ(a, b);
+}
+
+TEST(CRSMatrix, test_copy_2) {
+  std::vector<std::complex<double>> v = { 1, 2, 3, 4, 8, 5, 7, 1, 6 };
+  std::vector<int> c = { 0, 4, 2, 3, 3, 5, 1, 2, 5 };
+  std::vector<int> r = { 0, 2, 4, 4, 6, 6, 9 };
+
+  CRSMatrix a(6, 9, v, c, r);
+  CRSMatrix b(a);
+
+  EXPECT_EQ(a, b);
+}
+
+TEST(CRSMatrix, test_equally) {
+  std::vector<std::complex<double>> v = { 1, 2, 3, 4, 8, 5, 7, 1, 6 };
+  std::vector<int> c = { 0, 4, 2, 3, 3, 5, 1, 2, 5 };
+  std::vector<int> r = { 0, 2, 4, 4, 6, 6, 9 };
+
+  CRSMatrix a(6, 9, v, c, r);
+  CRSMatrix b;
+  b = a;
+
+  EXPECT_EQ(a, b);
+}
+
+TEST(CRSMatrix, test_transpose) {
+  std::vector<std::complex<double>> vt = { 1, 7, 3, 1, 4, 8, 2, 5, 6 };
+  std::vector<int> ct = { 0, 5, 1, 5, 1, 3, 0, 3, 5 };
+  std::vector<int> rt = { 0, 1, 2, 4, 6, 7, 9 };
+
+  std::vector<std::complex<double>> v = { 1, 2, 3, 4, 8, 5, 7, 1, 6 };
+  std::vector<int> c = { 0, 4, 2, 3, 3, 5, 1, 2, 5 };
+  std::vector<int> r = { 0, 2, 4, 4, 6, 6, 9 };
+
+  CRSMatrix a(6, 9, v, c, r);
+  CRSMatrix at(6, 9, vt, ct, rt);
+
+  EXPECT_EQ(at, a.transpose());
+}
+
+TEST(CRSMatrix, test_multiplicate) {
+  std::vector<std::complex<double>> vm = { 1, 2, 32, 20, 35, 5, 64, 70, 42, 27, 28, 36 };
+  std::vector<int> cm = { 0, 4, 3, 5, 1, 2, 3, 5, 1, 2, 3, 5 };
+  std::vector<int> rm = { 0, 2, 4, 4, 8, 8, 12 };
+
+  std::vector<std::complex<double>> v = { 1, 2, 3, 4, 8, 5, 7, 1, 6 };
+  std::vector<int> c = { 0, 4, 2, 3, 3, 5, 1, 2, 5 };
+  std::vector<int> r = { 0, 2, 4, 4, 6, 6, 9 };
+
+  CRSMatrix a(6, 9, v, c, r);
+  CRSMatrix b(6, 9, v, c, r);
+  CRSMatrix ab(6, 12, vm, cm, rm);
+
+  EXPECT_EQ(ab, a * b);
+}
+
+/*TEST(CRSMatrix, test_build) {
+    CRSMatrix a(2000, 20000);
+    CRSMatrix b(2000, 20000);
+
+    a.buildRandomCRSMatrix();
+    b.buildRandomCRSMatrix();
+
+    a.getThreads(1);
+    double start1_time = omp_get_wtime();
+    a * b;
+    double end1_time = omp_get_wtime();
+    std::cout << "Lin time: " << (end1_time - start1_time) << std::endl;
+
+    a.getThreads(4);
+    double start2_time = omp_get_wtime();
+    a * b;
+    double end2_time = omp_get_wtime();
+    std::cout << "Par time: " << (end2_time - start2_time) << std::endl;
+
+    EXPECT_EQ(0, 0);
+}*/
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
